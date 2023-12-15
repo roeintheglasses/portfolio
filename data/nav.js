b6f69e9183aa60760ae6a7f7f1f2ae1b6123f733
roeintheglasses <hrkjangir@gmail.com>
2023-12-15 21:43:59 +0530

    Minor Changes

--- data/nav.js
+++ data/nav.js
@@ -2,11 +2,9 @@ export const navigation = {
   pages: [
     { name: 'Home', href: '/' },
     { name: 'About', href: '/about' },
-    { name: 'Timeline', href: '/timeline' },
     { name: 'Guestbook', href: '/guestbook' },
     { name: 'Projects', href: '/projects' },
     { name: 'Snippets', href: '/snippets' },
-    { name: 'Anime', href: '/anime' },
     { name: 'Games', href: '/games' },
     { name: 'Contact', href: '/contact' }
   ]
--- pages/guestbook.tsx
+++ pages/guestbook.tsx
@@ -6,15 +6,15 @@ export default function GuestbookPage({ fallbackData }) {
   return (
     <Container
       title="Guestbook - Hrishikesh Jangir"
-      description="Sign my digital guestbook and share some wisdom."
+      description="Leave your mark here. Sign my digital guestbook and share some wisdom."
     >
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           Guestbook
         </h1>
         <p className="text-gray-600 dark:text-gray-400 mb-4">
-          Leave a comment below. It could be anything – appreciation,
-          information, wisdom, or even humor. Surprise me!
+          Leave a note below. It could be anything - appreciation, information,
+          recommendation, or even humor. Surprise me!
         </p>
         <Guestbook fallbackData={fallbackData} />
       </div>
