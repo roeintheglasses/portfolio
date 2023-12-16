e53348f8550a6bf5c58e98f12d81d2f8849b39db
roeintheglasses <hrkjangir@gmail.com>
2023-12-16 23:04:23 +0530

    content update

--- data/workEx.js
+++ data/workEx.js
@@ -2,7 +2,8 @@ export const workEx = {
   adpushup: {
     role: 'SDE',
     company: 'Adpushup',
-    companylogo: import('public/static/images/companies/Adpushup.jpg'),
+    companylogo:
+      'https://roeintheglasses.tech/static/images/companies/Adpushup.jpg',
     color: '#ff4954',
     date: 'Auguest 2021 - Present',
     desc: 'AdPushup is a revenue optimization platform that helps publishers increase their ad revenue. At Adpushup I Lead a team of 4 engineers working on the framerate video player that serves instream video ads at scale.'
@@ -10,7 +11,8 @@ export const workEx = {
   quikieApps: {
     role: 'SDE',
     company: 'Quikie Apps',
-    companylogo: import('public/static/images/companies/quikieApps.jpg'),
+    companylogo:
+      'https://roeintheglasses.tech/static/images/companies/quikieApps.jpg',
     color: '#6fc8e8',
     date: 'August 2020 - July 2021',
     desc: 'QuikieApps is a IT services and consulting company specialzing in fullstack development. Here I Worked as an SDE on multiple MERN Stack & React Native Projects for various different Clients.'
@@ -18,7 +20,8 @@ export const workEx = {
   safe: {
     role: 'Testing Intern',
     company: 'Safe Security',
-    companylogo: import('public/static/images/companies/safe.jpg'),
+    companylogo:
+      'https://roeintheglasses.tech/static/images/companies/safe.jpg',
     color: '#141c4a',
     date: 'March 2019 - July 2019',
     desc: 'Safe Security is Cybersecurity and Digital Business Risk Assesment platform that helps organizations measure and mitigate enterprise-wide cyber risk in real-time. At Safe I worked as an testing intern and automated their core test cases.'
