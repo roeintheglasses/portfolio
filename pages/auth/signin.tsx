49dd3a478d3397e7926aa2485aab2bd25f49a26b
roeintheglasses <hrkjangir@gmail.com>
2024-01-03 02:24:08 +0530

    Minor font change

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -62,7 +62,7 @@ export default function SignIn({
                   <div key={provider.name}>
                     <button
                       onClick={() => signIn(provider.id)}
-                      className="w-full flex flex-row gap-2 tracking-tight leading-tight font-semibold justify-center items-center text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      className="w-full flex flex-row gap-2 tracking-tight leading-tight font-semibold justify-center items-center text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-sm px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
                       {' '}
                       <Icon key={provider.id} />
