93634567d3784574714a38399a70f4113eb91ccf
roeintheglasses <hrkjangir@gmail.com>
2024-01-26 05:11:23 +0530

    Added Shad/cn UI & Updated ChipStacks for mobile

--- components/ChipStack/ChipStack.tsx
+++ components/ChipStack/ChipStack.tsx
@@ -1,18 +1,47 @@
 import Chip from './Chip';
 
 import { ChipData } from 'lib/types';
+import { Badge } from '../Badge';
+
+import cn from 'classnames';
+export function BadgeDemo() {
+  return <Badge>Badge</Badge>;
+}
 
 export default function ChipStack({ chipList }) {
   return (
-    <div className="flex flex-row justify-center flex-wrap">
-      {chipList.map((chip: ChipData, index: number) => (
-        <Chip
-          chipName={chip.chipName}
-          gradient={chip.gradient}
-          ChipIcon={chip.icon}
-          key={index}
-        />
-      ))}
-    </div>
+    <>
+      <div className="hidden md:flex flex-row justify-center flex-wrap">
+        {chipList.map((chip: ChipData, index: number) => (
+          <Chip
+            chipName={chip.chipName}
+            gradient={chip.gradient}
+            ChipIcon={chip.icon}
+            key={index}
+          />
+        ))}
+      </div>
+      <div className="mt-10 flex flex-row justify-center flex-wrap gap-4 md:hidden">
+        {chipList.map((chip: ChipData, index: number) => (
+          <div
+            key={index}
+            className={cn(
+              'p-0.5 rounded-full animate-gradient-x bg-gradient-to-r',
+              chip.gradient
+            )}
+          >
+            <Badge
+              className={cn(
+                'text-base bg-gray-50 dark:bg-gray-900 bg-opacity-95 dark:bg-opacity-95'
+              )}
+              variant="secondary"
+            >
+              <chip.icon className="inline-block w-4 h-4 mr-2" />
+              {chip.chipName}
+            </Badge>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@ const { spacing, fontFamily, screens } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ['./pages/**/*.tsx', './components/**/*.tsx', './layouts/**/*.tsx'],
-  darkMode: 'class',
+  darkMode: ['class'],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px'
+      }
+    },
     extend: {
       colors: {
         'blue-opaque': 'rgb(13 42 148 / 18%)',
@@ -18,7 +25,45 @@ module.exports = {
           700: '#333333',
           800: '#222222',
           900: '#000000'
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
         }
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: 'calc(var(--radius) - 4px)'
       },
       fontFamily: {
         sans: ['IBM Plex Sans', ...fontFamily.sans]
@@ -99,9 +144,19 @@ module.exports = {
       animation: {
         'gradient-x': 'gradient-x 15s ease infinite',
         'gradient-y': 'gradient-y 15s ease infinite',
-        'gradient-xy': 'gradient-xy 15s ease infinite'
+        'gradient-xy': 'gradient-xy 15s ease infinite',
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
       },
       keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' }
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' }
+        },
         'gradient-y': {
           '0%, 100%': {
             'background-size': '400% 400%',
@@ -138,5 +193,5 @@ module.exports = {
   variants: {
     typography: ['dark']
   },
-  plugins: [require('@tailwindcss/typography')]
+  plugins: [require('@tailwindcss/typography'), require('tailwindcss-animate')]
 };
